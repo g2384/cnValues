ff70bd7acdcf06c864c7a5d2e1166987a51978cc
g2384 <g2384@users.noreply.github.com>
2020-08-29 17:13:51 +0100

    upload

---
+++ quiz_files/specials.js
@@ -0,0 +1,97 @@
+specials = [
+    {
+        "id": "ruguan",
+        "name": "入关学",
+        "desc": NaN
+    },
+    {
+        "id": "yinmou",
+        "name": "阴谋论",
+        "desc": NaN
+    },
+    {
+        "id": "onegod",
+        "name": "一神论",
+        "desc": NaN
+    },
+    {
+        "id": "rujia",
+        "name": "儒家",
+        "desc": NaN
+    },
+    {
+        "id": "gongye",
+        "name": "工业党",
+        "desc": NaN
+    },
+    {
+        "id": "mao",
+        "name": "毛粉",
+        "desc": NaN
+    },
+    {
+        "id": "han",
+        "name": "皇汉",
+        "desc": NaN
+    },
+    {
+        "id": "jieti",
+        "name": "解体论",
+        "desc": NaN
+    },
+    {
+        "id": "minguo",
+        "name": "民国派",
+        "desc": NaN
+    },
+    {
+        "id": "nvquan",
+        "name": "女权主义",
+        "desc": NaN
+    },
+    {
+        "id": "suijing",
+        "name": "政治冷感",
+        "desc": NaN
+    },
+    {
+        "id": "dengta",
+        "name": "全盘西化",
+        "desc": NaN
+    },
+    {
+        "id": "nimin",
+        "name": "逆向民族主义",
+        "desc": NaN
+    },
+    {
+        "id": "difang",
+        "name": "地方主义",
+        "desc": NaN
+    },
+    {
+        "id": "sheda",
+        "name": "社会达尔文主义",
+        "desc": NaN
+    },
+    {
+        "id": "tese",
+        "name": "中国特色社会主义",
+        "desc": NaN
+    },
+    {
+        "id": "haidao",
+        "name": "海盗党",
+        "desc": NaN
+    },
+    {
+        "id": "jiasu",
+        "name": "加速主义",
+        "desc": NaN
+    },
+    {
+        "id": "shequn",
+        "name": "社群主义",
+        "desc": NaN
+    }
+];
